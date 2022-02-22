482a09469ee5f6fc1ff60ffbe6b5ea2b29ab68bc
Gha-ith <ghaithimad@gmail.com>
2022-02-22 01:17:03 +0000

    W6D1

--- app/actions.rb
+++ app/actions.rb
@@ -23,9 +23,7 @@ post '/signup' do
     erb(:signup)
   end
 
-  
-
-  @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
+   @user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password })
 
   if @user.save
     "User #{username} saved!"
@@ -82,10 +80,32 @@ post '/finstagram_posts' do
 end
 
 get '/finstagram_posts/:id' do
-  @finstagram_post = FinstagramPost.find(params[:id])   # find the finstagram post with the ID from the URL
-  erb(:"finstagram_posts/show")               # render app/views/finstagram_posts/show.erb
+  @finstagram_post = FinstagramPost.find(params[:id])   
+  erb(:"finstagram_posts/show")               
 end
 
+post '/comments' do
+  text = params[:text]
+  finstagram_post_id = params[:finstagram_post_id]
+  comment = Comment.new({ text: text, finstagram_post_id: finstagram_post_id, user_id: current_user.id })
+  comment.save
+  redirect(back)
+end
+
+post '/likes' do
+  finstagram_post_id = params[:finstagram_post_id]
+
+  like = Like.new({ finstagram_post_id: finstagram_post_id, user_id: current_user.id })
+  like.save
+
+  redirect(back)
+end
+
+delete '/likes/:id' do
+  like = Like.find(params[:id])
+  like.destroy
+  redirect(back)
+end
 
 
 
